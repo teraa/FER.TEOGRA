078c4c3c32110ce38668eb6fa00ca0fefc8e0ae6
Tera <24725862+teraa@users.noreply.github.com>
2021-05-25 00:26:10 +0200

    begin impl

--- Zad1/Program.cs
+++ Zad1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 
 namespace TEOGRA.Zad1
 {
@@ -6,7 +7,66 @@ namespace TEOGRA.Zad1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line = Console.ReadLine() ?? throw new FormatException();
+
+            int n = int.Parse(line);
+            bool[][] m = new bool[n][];
+
+            Console.ReadLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                line = Console.ReadLine() ?? throw new FormatException();
+
+                m[i] = new bool[n];
+                for (int j = 0; j < n; j++)
+                {
+                    m[i][j] = line[j * 2] switch
+                    {
+                        '0' => false,
+                        '1' => true,
+                        _ => throw new FormatException(),
+                    };
+                }
+            }
+#if DEBUG
+            // Verify Matrix
+            Debug.Assert(m.Length == n);
+
+            for (int i = 0; i < n; i++)
+            {
+                Debug.Assert(m[i].Length == n);
+
+                for (int j = i + 1; j < n; j++)
+                    Debug.Assert(m[i][j] == m[j][i]);
+            }
+#endif
+
+            int result = FindLongestChain(n, m);
+            Console.WriteLine(result);
+        }
+
+        static int FindLongestChain(int n, bool[][] m)
+        {
+            int max = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    int tmp = FindLongestChain(n, i, j, m);
+                    if (max < tmp)
+                        max = tmp;
+                }
+            }
+
+            return max;
+        }
+
+        static int FindLongestChain(int n, int i, int j, bool[][] m)
+        {
+            Console.WriteLine($"({i}, {j})");
+            return 0;
         }
     }
 }
