5789175820f0820d35c0f4e1f0d2bc9480dcae0d
Tera <24725862+teraa@users.noreply.github.com>
2021-05-24 23:11:20 +0200

    initial commit

---
+++ Zad1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEOGRA.Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
