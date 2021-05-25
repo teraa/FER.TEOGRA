9e4422c9a5887091a7cd999c7fefaa32c5b1e639
Tera <24725862+teraa@users.noreply.github.com>
2021-05-25 15:01:20 +0200

    rename var

--- Zad1/Program.cs
+++ Zad1/Program.cs
@@ -11,19 +11,19 @@ namespace TEOGRA.Zad1
             string line = Console.ReadLine() ?? throw new FormatException();
 
             int n = int.Parse(line);
-            bool[][] m = new bool[n][];
+            bool[][] a = new bool[n][];
 
             Console.ReadLine();
 
-            for (int i = 0; i < m.Length; i++)
+            for (int i = 0; i < a.Length; i++)
             {
                 line = Console.ReadLine() ?? throw new FormatException();
                 string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-                m[i] = new bool[m.Length];
-                for (int j = 0; j < m[i].Length; j++)
+                a[i] = new bool[a.Length];
+                for (int j = 0; j < a[i].Length; j++)
                 {
-                    m[i][j] = parts[j] switch
+                    a[i][j] = parts[j] switch
                     {
                         "0" => false,
                         "1" => true,
@@ -34,18 +34,18 @@ namespace TEOGRA.Zad1
 
 #if DEBUG
             // Verify Matrix
-            Debug.Assert(m.Length == n);
+            Debug.Assert(a.Length == n);
 
             for (int i = 0; i < n; i++)
             {
-                Debug.Assert(m[i].Length == n);
+                Debug.Assert(a[i].Length == n);
 
                 for (int j = i + 1; j < n; j++)
-                    Debug.Assert(m[i][j] == m[j][i]);
+                    Debug.Assert(a[i][j] == a[j][i]);
             }
 #endif
 
-            int[] result = FindLongestChain(m);
+            int[] result = FindLongestChain(a);
             for (int i = 0; i < result.Length; i++)
                 result[i]++;
             Console.WriteLine(string.Join(", ", result));
@@ -53,13 +53,13 @@ namespace TEOGRA.Zad1
             Console.WriteLine(result.Length - 1);
         }
 
-        static int[] FindLongestChain(bool[][] m)
+        static int[] FindLongestChain(bool[][] a)
         {
             int[] max = Array.Empty<int>();
 
-            for (int i = 0; i < m.Length; i++)
+            for (int i = 0; i < a.Length; i++)
             {
-                int[] tmp = FindLongestChain(i, new int[] { i }, m);
+                int[] tmp = FindLongestChain(i, new int[] { i }, a);
                 if (max.Length < tmp.Length)
                     max = tmp;
             }
@@ -67,20 +67,20 @@ namespace TEOGRA.Zad1
             return max;
         }
 
-        static int[] FindLongestChain(int v, int[] visited, bool[][] m)
+        static int[] FindLongestChain(int v, int[] visited, bool[][] a)
         {
             int[] max = visited;
 
-            for (int i = 0; i < m.Length; i++)
+            for (int i = 0; i < a.Length; i++)
             {
-                if (i == v || visited.Contains(i) || !m[v][i])
+                if (i == v || visited.Contains(i) || !a[v][i])
                     continue;
 
                 int[] newVisited = new int[visited.Length + 1];
                 visited.CopyTo(newVisited, 0);
                 newVisited[^1] = i;
 
-                int[] tmp = FindLongestChain(i, newVisited, m);
+                int[] tmp = FindLongestChain(i, newVisited, a);
                 if (max.Length < tmp.Length)
                     max = tmp;
             }
