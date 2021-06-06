1e3eed5f46a7fe3fa57c99bfb1abc51946b05755
Tera <24725862+teraa@users.noreply.github.com>
2021-06-06 22:02:56 +0200

    add tests

---
+++ Zad1.Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+using System;
+using Xunit;
+using static TEOGRA.Zad1.Program;
+
+namespace TEOGRA.Zad1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_Graf()
+        {
+            bool[][] a = new bool[][]
+            {
+                new [] { false, true, true, false },
+                new [] { true, false, true, true },
+                new [] { true, true, false, true },
+                new [] { false, true, true, false },
+            };
+
+            int[] result = FindLongestChain(a);
+            Assert.Equal(3, result.Length - 1);
+        }
+
+        [Fact]
+        public void Test_Graf_Stablo()
+        {
+            bool[][] a = new bool[][]
+            {
+                new[] { false, true, false, false, false, false, false, false },
+                new[] { true, false, true, true, false, false, false, false },
+                new[] { false, true, false, true, true, false, false, false },
+                new[] { false, true, true, false, false, false, false, false },
+                new[] { false, false, true, false, false, true, false, false },
+                new[] { false, false, false, false, true, false, true, true },
+                new[] { false, false, false, false, false, true, false, false },
+                new[] { false, false, false, false, false, true, false, false },
+            };
+
+            int[] result = FindLongestChain(a);
+            Assert.Equal(6, result.Length - 1);
+        }
+
+        [Fact]
+        public void Test_Graf_Peterson()
+        {
+            bool[][] a = new bool[][]
+            {
+                new[] { false, true, true, false, false, true, false, false, false, false },
+                new[] { true, false, false, true, false, false, true, false, false, false },
+                new[] { true, false, false, false, true, false, false, true, false, false },
+                new[] { false, true, false, false, true, false, false, false, true, false },
+                new[] { false, false, true, true, false, false, false, false, false, true },
+                new[] { true, false, false, false, false, false, false, false, true, true },
+                new[] { false, true, false, false, false, false, false, true, false, true },
+                new[] { false, false, true, false, false, false, true, false, true, false },
+                new[] { false, false, false, true, false, true, false, true, false, false },
+                new[] { false, false, false, false, true, true, true, false, false, false },
+            };
+
+            int[] result = FindLongestChain(a);
+            Assert.Equal(9, result.Length - 1);
+        }
+    }
+}
