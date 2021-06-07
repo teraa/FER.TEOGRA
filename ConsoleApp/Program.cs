e720fd995da0b7bee2f2eadc13fc18f73da8620b
Tera <24725862+teraa@users.noreply.github.com>
2021-06-07 20:06:47 +0200

    add example project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Gen(new int[3], 0, 3);
+
+static void Gen(int[] digits, int idx, int n)
+{
+    if (idx + 1 == digits.Length)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            digits[idx] = i;
+            Console.WriteLine(string.Join("", digits));
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            digits[idx] = i;
+            Gen(digits, idx + 1, n);
+        }
+    }
+}
