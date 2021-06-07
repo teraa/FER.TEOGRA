615d7765f59002b4bf4d4bda361e900bb696a881
Tera <24725862+teraa@users.noreply.github.com>
2021-06-07 13:39:35 +0200

    add projects

---
+++ Zad2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEOGRA.Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
